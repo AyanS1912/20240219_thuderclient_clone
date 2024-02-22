7030698c31dfce484458cc9e2475b6cf9b265cf8
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-02-22 10:15:22 +0530

    CSS added for quey buttons

--- 20240219_thunderclient_clone/thunder.js
+++ 20240219_thunderclient_clone/thunder.js
@@ -10,7 +10,7 @@ function QuerySection() {
             <button class="add-kv" onclick="addKeyValuePair()">+</button>
         </div>
     `;
-    requestType.innerHTML = ''; // Clear previous content
+    requestType.innerHTML = ''; 
     requestType.appendChild(keyValueDiv);
 }
 
@@ -29,4 +29,52 @@ function addKeyValuePair() {
 
 function removeKeyValuePair(button) {
     button.parentNode.remove();
+}
+
+function fetchData(){
+
+    let method = document.getElementById('http-methods-data').value
+    let url =  document.getElementById('URL-data').value
+    console.log(method,url)
+    const start = performance.now()
+    fetch(url,{
+        method:method,
+        mode : "cors",
+        headers: {
+            "Content-Type": "application/json",
+          }
+    })
+    .then(response => {
+        // console.log(response.json())
+        const status = document.getElementById("status-data")
+        status.innerHTML = `Status : ${response.status}`
+        status.style.color = 'green'
+
+        const response_data = document.getElementById("response-data")
+        response_data.innerHTML = `<p>${response.data}</p>`
+
+        const text = response.text(); // Get response body as text
+        const responseSize = new Blob([text]).size;
+        const size_taken =  document.getElementById("size-data")
+        console.log(responseSize)
+        size_taken.innerHTML = ` Size: ${responseSize} Byte`
+        size_taken.style.color = 'green'
+
+        console.log(response.json())
+        return response.json()
+    })
+    .then(data =>{
+        console.log(data)
+    
+    }).catch(error => {
+        console.error(error)
+    })
+    const end = performance.now()
+    const time = (end - start).toFixed(3)
+
+    const time_taken = document.getElementById("time-data")
+    time_taken.innerHTML = `Time: ${time} ms`
+    time_taken.style.color = 'green'
+    // console.log(Math.round(time)+"ms")
+
 }
